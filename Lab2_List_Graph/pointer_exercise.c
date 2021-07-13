67d0239208462728b582b12cce05b6c817781612
190322234 <62374056+190322234@users.noreply.github.com>
2021-07-13 20:24:05 +0800

    lesson 3

---
+++ Lab2_List_Graph/pointer_exercise.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include<stdlib.h>
+void swap(int *,int *);
+int main(void)
+{
+  int a=4,b=3;  
+  swap(&a,&b);  
+  printf("a = %d  b= %d\n",a,b);
+  system("pause");     
+}
+void swap(int *x,int *y)
+{
+  int tmp;
+  tmp = *x;
+  *x = *y;
+  *y = tmp;
+}
